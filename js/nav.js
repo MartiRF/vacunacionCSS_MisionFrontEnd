df346c04fc2c81c95245dcfa8b96806e2892f6fe
Martin Reyes <martinreyesrf@gmail.com>
2022-11-12 21:26:30 -0600

    commit #2

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,9 @@
 let botonMenuRef = document.getElementById('btn_menu') 
+let menuRef = document.getElementById('btns-options')
+
+botonMenuRef.addEventListener('click',() => {
+    menuRef.style.display = 'flex'
+})
+menuRef.addEventListener('click', () => {
+    menuRef.style.display = 'none'
+})
